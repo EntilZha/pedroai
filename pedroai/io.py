83ac2f3fe2630be46ea08e1ca92060e7529e33f5
Pedro Rodriguez <me@pedro.ai>
2020-07-30 20:10:30 -0400

    Add safe_file and republish

--- pedroai/io.py
+++ pedroai/io.py
@@ -10,16 +10,25 @@ def eprint(*args, **kwargs):
 
 
 def read_json(path: str):
+    """
+    Read a json file from a string path
+    """
     with open(path) as f:
         return json.load(f)
 
 
 def write_json(path: str, obj: Any):
+    """
+    Write an object to a string path as json
+    """
     with open(path, "w") as f:
         json.dump(obj, f)
 
 
 def _read_jsonlines_list(path: str):
+    """
+    Read a jsonlines file into memory all at once
+    """
     out = []
     with open(path) as f:
         for line in f:
@@ -28,12 +37,18 @@ def _read_jsonlines_list(path: str):
 
 
 def _read_jsonlines_lazy(path: str):
+    """
+    Lazily return the contents of a jsonlines file
+    """
     with open(path) as f:
         for line in f:
             yield json.loads(line)
 
 
 def read_jsonlines(path: str, lazy: bool = False):
+    """
+    Read a jsonlines file as a list/iterator of json objects
+    """
     if lazy:
         return _read_jsonlines_lazy(path)
     else:
@@ -41,6 +56,9 @@ def read_jsonlines(path: str, lazy: bool = False):
 
 
 def write_jsonlines(path: str, elements: List[Any]):
+    """
+    Write a list of json serialiazable objects to the path given
+    """
     with open(path, "w") as f:
         for e in elements:
             f.write(json.dumps(e))
@@ -69,3 +87,14 @@ class requires_file:
                 pass
 
             return nop
+
+
+def safe_file(path: str) -> str:
+    """
+    Ensure that the path to the file exists, then return the path.
+    
+    For example, if the path passed in is /home/entilzha/stuff/stuff/test.txt,
+    this function will run the equivalent of mkdir -p /home/entilzha/stuff/stuff/
+    """
+    os.makedirs(os.path.dirname(path), exist_ok=True)
+    return path
